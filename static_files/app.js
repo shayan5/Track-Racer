da77143016f6d34f53b75448e4872a3fc03458a1
shayan5 <25065328+shayan5@users.noreply.github.com>
2020-03-28 22:37:45 -0400

    implemented rotations

--- static_files/app.js
+++ static_files/app.js
@@ -1,13 +1,17 @@
 
 //https://stackoverflow.com/questions/11127543/how-to-move-an-object-forward-in-three-js
 
-let playerAngle = 90;
+//let playerAngle = 0;
+let pressed = {};
+let clock = new THREE.Clock();
 
 
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xa0c1f5);
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 0.1, 1000 );
+const camera = 
+new THREE.PerspectiveCamera( 
+	45, window.innerWidth/window.innerHeight, 0.1, 1000 );
 //camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -25,9 +29,10 @@ const geometry = new THREE.PlaneGeometry(1, 1);
 const material = new THREE.MeshBasicMaterial( { color: 0x000000 } );
 const player = new THREE.Mesh( geometry, material );
 player.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 2);
+player.rotateOnAxis(new THREE.Vector3(0, 1, 0), Math.PI);
 player.position.x = 0; 
-player.position.y = 1.5;
-player.position.z = 0;
+player.position.y = 0;
+player.position.z = 0.5;
 player.add(camera); //camera will follow player
 scene.add( player );
 
@@ -45,6 +50,7 @@ const wallGeometry = new THREE.PlaneGeometry( 5, 10 );
 const wallMaterial = new THREE.MeshBasicMaterial( {color: 0xff0000, side: THREE.DoubleSide} );
 const plane1 = new THREE.Mesh( wallGeometry, wallMaterial );
 plane1.position.y = 50;
+plane1.position.z = 5;
 plane1.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 2);
 scene.add( plane1 );
 
@@ -59,11 +65,56 @@ const animate = function () {
 
 	camera.position.z = 5;
 	renderer.render( scene, camera );
+	movePlayer();
 };
 
 animate();
+addListeners();
 
+
+//records pressed keys
+function addListeners() {
+    window.addEventListener('keydown', function(e) {
+      pressed[e.key.toUpperCase()] = true;
+    })
+    window.addEventListener('keyup', function(e) {
+      pressed[e.key.toUpperCase()] = false;
+    })
+}
+
+function movePlayer() {
+    var delta = clock.getDelta(); // interval 1/60th of a second
+    var moveDistance = 10 * delta; // 10 pixels per second
+    var rotateAngle = Math.PI * delta; // pi radians (180 deg) per interval
+
+    // move forwards/backwards/left/right
+    if ( pressed['W'] ) {
+	  //player.rotateOnAxis(new THREE.Vector3(1,0,0), -rotateAngle)
+	  //console.log(player.position.x + ", " + player.position.y);
+      player.translateZ( -1 * moveDistance );
+    }
+    if ( pressed['S'] ) 
+      player.translateZ(  moveDistance );
+    if ( pressed['A'] )
+      player.translateX( -1 * moveDistance );
+    if ( pressed['D'] )
+	  player.translateX(  moveDistance ); 
+	  
+	var rotation_matrix = new THREE.Matrix4().identity();
+    if ( pressed['A'] )
+      player.rotateOnAxis(new THREE.Vector3(0,1,0), rotateAngle);
+    if ( pressed['D'] )
+      player.rotateOnAxis(new THREE.Vector3(0,1,0), -rotateAngle);
+
+  }
+
+
+/*
 document.onkeydown = checkKey;
+
+
+
+
 
 function checkKey(e) {
 
@@ -71,23 +122,23 @@ function checkKey(e) {
 
     if (e.keyCode == '38') {
 		// up arrow
-		console.log(player.position.x + ", " + player.position.y + ", " + player.position.z);
-		player.position.x = Math.cos(playerAngle * Math.PI / 180) + player.position. x;
-		player.position.y = Math.sin(playerAngle * Math.PI / 180) + player.position.y;
-    }
+		//player.translateX(1);
+		player.translateZ(-1);
+		console.log(player.position.x, ", " + player.position.y);
+	}
     else if (e.keyCode == '40') {
 		// down arrow
 		player.position.y -= 1;
+		player.translateZ(1);
     }
     else if (e.keyCode == '37') {
 	   // left arrow
-	   player.rotation.y += 0.1;
-	   playerAngle += 1;
+	   player.rotation.y -= 0.1;
     }
     else if (e.keyCode == '39') {
 	   // right arrow
-	   player.rotation.y -= 0.1;
-	   playerAngle -= 1;
+	   player.rotation.y += 0.1;
     }
 
 }
+*/
