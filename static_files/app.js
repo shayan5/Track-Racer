25bd5ceb241e6575f191fb9dfb2981c429c70cb1
shayan5 <25065328+shayan5@users.noreply.github.com>
2020-03-29 11:09:51 -0400

    scene and background drawn

--- static_files/app.js
+++ static_files/app.js
@@ -8,7 +8,7 @@ let clock = new THREE.Clock();
 
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xa0c1f5);
+scene.background = new THREE.Color(0xab6a8c);
 const camera = 
 new THREE.PerspectiveCamera( 
 	45, window.innerWidth/window.innerHeight, 0.1, 1000 );
@@ -45,6 +45,175 @@ const ground = new THREE.Mesh(groundGeometry, groundMaterial)
 scene.add(ground);
 */
 
+
+//load map textures
+//northern skybox
+let bgTexture = new THREE.TextureLoader().load('level/bg1.png');
+let bgMaterial = new THREE.MeshBasicMaterial({map: bgTexture});
+let bgPlane = new THREE.PlaneGeometry(500, 130);
+const northernBgMesh = new THREE.Mesh(bgPlane, bgMaterial);
+northernBgMesh.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 2);
+northernBgMesh.position.x = 0;
+northernBgMesh.position.y = 250;
+northernBgMesh.position.z = 65;
+scene.add(northernBgMesh);
+
+//northern mountains
+let mtTexture = new THREE.TextureLoader().load('level/mountain2.png');
+mtTexture.wrapS = THREE.RepeatWrapping;
+mtTexture.repeat.x = 2;
+let mtMaterial = new THREE.MeshBasicMaterial({map: mtTexture});
+mtMaterial.transparent = true;
+let mtPlane = new THREE.PlaneGeometry(300, 60);
+const northernMtMesh = new THREE.Mesh(mtPlane, mtMaterial);
+northernMtMesh.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 2);
+northernMtMesh.position.x = 0;
+northernMtMesh.position.y = 150;
+northernMtMesh.position.z = 30;
+scene.add(northernMtMesh);
+
+//northern trees
+let trTexture = new THREE.TextureLoader().load('level/trees1.png');
+trTexture.wrapS = THREE.RepeatWrapping;
+trTexture.repeat.x = 3;
+let trMaterial = new THREE.MeshBasicMaterial({map: trTexture});
+trMaterial.transparent = true;
+let trPlane = new THREE.PlaneGeometry(210, 20);
+const northernTrMesh = new THREE.Mesh(trPlane, trMaterial);
+northernTrMesh.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 2);
+northernTrMesh.position.x = 0;
+northernTrMesh.position.y = 105;
+northernTrMesh.position.z = 10;
+scene.add(northernTrMesh);
+
+
+//southern skybox
+bgTexture = new THREE.TextureLoader().load('level/bg2.png');
+bgMaterial = new THREE.MeshBasicMaterial({map: bgTexture});
+bgPlane = new THREE.PlaneGeometry(500, 130);
+const southernBgMesh = new THREE.Mesh(bgPlane, bgMaterial);
+southernBgMesh.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 2);
+southernBgMesh.rotateOnAxis(new THREE.Vector3(0, 1, 0), Math.PI);
+southernBgMesh.position.x = 0;
+southernBgMesh.position.y = -250;
+southernBgMesh.position.z = 65;
+scene.add(southernBgMesh);
+
+//southern mountains
+mtTexture = new THREE.TextureLoader().load('level/mountain2.png');
+mtTexture.wrapS = THREE.RepeatWrapping;
+mtTexture.repeat.x = 2;
+mtMaterial = new THREE.MeshBasicMaterial({map: mtTexture});
+mtMaterial.transparent = true;
+mtPlane = new THREE.PlaneGeometry(300, 60);
+const southernMtMesh = new THREE.Mesh(mtPlane, mtMaterial);
+southernMtMesh.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 2);
+southernMtMesh.rotateOnAxis(new THREE.Vector3(0, 1, 0), Math.PI);
+southernMtMesh.position.x = 0;
+southernMtMesh.position.y = -150;
+southernMtMesh.position.z = 30;
+scene.add(southernMtMesh);
+
+//southern trees
+trTexture = new THREE.TextureLoader().load('level/trees1.png');
+trTexture.wrapS = THREE.RepeatWrapping;
+trTexture.repeat.x = 3;
+trMaterial = new THREE.MeshBasicMaterial({map: trTexture});
+trMaterial.transparent = true;
+trPlane = new THREE.PlaneGeometry(210, 20);
+const southernTrMesh = new THREE.Mesh(trPlane, trMaterial);
+southernTrMesh.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 2);
+southernTrMesh.rotateOnAxis(new THREE.Vector3(0, 1, 0), Math.PI);
+southernTrMesh.position.x = 0;
+southernTrMesh.position.y = -105;
+southernTrMesh.position.z = 10;
+scene.add(southernTrMesh);
+
+//eastern skybox
+bgTexture = new THREE.TextureLoader().load('level/bg2.png');
+bgMaterial = new THREE.MeshBasicMaterial({map: bgTexture});
+bgPlane = new THREE.PlaneGeometry(500, 130);
+const easternBgMesh = new THREE.Mesh(bgPlane, bgMaterial);
+easternBgMesh.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 2);
+easternBgMesh.rotateOnAxis(new THREE.Vector3(0, 1, 0), 3 * Math.PI / 2);
+easternBgMesh.position.x = 250;
+easternBgMesh.position.y = 0;
+easternBgMesh.position.z = 65;
+scene.add(easternBgMesh);
+
+//eastern mountains
+mtTexture = new THREE.TextureLoader().load('level/mountain2.png');
+mtTexture.wrapS = THREE.RepeatWrapping;
+mtTexture.repeat.x = 2;
+mtMaterial = new THREE.MeshBasicMaterial({map: mtTexture});
+mtMaterial.transparent = true;
+mtPlane = new THREE.PlaneGeometry(300, 60);
+const easternMtMesh = new THREE.Mesh(mtPlane, mtMaterial);
+easternMtMesh.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 2);
+easternMtMesh.rotateOnAxis(new THREE.Vector3(0, 1, 0), 3 * Math.PI / 2);
+easternMtMesh.position.x = 150;
+easternMtMesh.position.y = 0;
+easternMtMesh.position.z = 30;
+scene.add(easternMtMesh);
+
+//eastern trees
+trTexture = new THREE.TextureLoader().load('level/trees1.png');
+trTexture.wrapS = THREE.RepeatWrapping;
+trTexture.repeat.x = 3;
+trMaterial = new THREE.MeshBasicMaterial({map: trTexture});
+trMaterial.transparent = true;
+trPlane = new THREE.PlaneGeometry(210, 20);
+const easternTrMesh = new THREE.Mesh(trPlane, trMaterial);
+easternTrMesh.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 2);
+easternTrMesh.rotateOnAxis(new THREE.Vector3(0, 1, 0), 3 * Math.PI / 2);
+easternTrMesh.position.x = 105;
+easternTrMesh.position.y = 0;
+easternTrMesh.position.z = 10;
+scene.add(easternTrMesh);
+
+//western skybox
+bgTexture = new THREE.TextureLoader().load('level/bg2.png');
+bgMaterial = new THREE.MeshBasicMaterial({map: bgTexture});
+bgPlane = new THREE.PlaneGeometry(500, 130);
+const westernBgMesh = new THREE.Mesh(bgPlane, bgMaterial);
+westernBgMesh.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 2);
+westernBgMesh.rotateOnAxis(new THREE.Vector3(0, 1, 0), Math.PI / 2);
+westernBgMesh.position.x = -250;
+westernBgMesh.position.y = 0;
+westernBgMesh.position.z = 65;
+scene.add(westernBgMesh);
+
+//western mountains
+mtTexture = new THREE.TextureLoader().load('level/mountain2.png');
+mtTexture.wrapS = THREE.RepeatWrapping;
+mtTexture.repeat.x = 2;
+mtMaterial = new THREE.MeshBasicMaterial({map: mtTexture});
+mtMaterial.transparent = true;
+mtPlane = new THREE.PlaneGeometry(300, 60);
+const westernMtMesh = new THREE.Mesh(mtPlane, mtMaterial);
+westernMtMesh.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 2);
+westernMtMesh.rotateOnAxis(new THREE.Vector3(0, 1, 0), Math.PI / 2);
+westernMtMesh.position.x = -150;
+westernMtMesh.position.y = 0;
+westernMtMesh.position.z = 30;
+scene.add(westernMtMesh);
+
+//western trees
+trTexture = new THREE.TextureLoader().load('level/trees1.png');
+trTexture.wrapS = THREE.RepeatWrapping;
+trTexture.repeat.x = 3;
+trMaterial = new THREE.MeshBasicMaterial({map: trTexture});
+trMaterial.transparent = true;
+trPlane = new THREE.PlaneGeometry(210, 20);
+const westernTrMesh = new THREE.Mesh(trPlane, trMaterial);
+westernTrMesh.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 2);
+westernTrMesh.rotateOnAxis(new THREE.Vector3(0, 1, 0), Math.PI / 2);
+westernTrMesh.position.x = -105;
+westernTrMesh.position.y = 0;
+westernTrMesh.position.z = 10;
+scene.add(westernTrMesh);
+
+
 //northern wall
 const wallGeometry = new THREE.PlaneGeometry( 5, 10 );
 const wallMaterial = new THREE.MeshBasicMaterial( {color: 0xff0000, side: THREE.DoubleSide} );
@@ -54,6 +223,9 @@ plane1.position.z = 5;
 plane1.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 2);
 scene.add( plane1 );
 
+//grid helper
+var axesHelper = new THREE.AxesHelper( 5 );
+scene.add( axesHelper );
 var gridHelper = new THREE.GridHelper( 100, 100 );
 gridHelper.rotateOnAxis(new THREE.Vector3(1, 0, 0), Math.PI / 2);
 scene.add( gridHelper );
