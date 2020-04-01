57e2cd0aed5bccc3afe4c0587e7ff514a4c3a58b
S A <25065328+shayan5@users.noreply.github.com>
2020-03-31 23:36:29 -0400

    colour selection functionality added

--- static_files/editor.js
+++ static_files/editor.js
@@ -1,9 +1,17 @@
 let colour = 1
 let map = null;
+let record = false;
+let colourMap = {
+    1: '#000000', //boundary
+    2: '#36b404', //grass
+    3: '#586454', //track
+    4: '#ffffff', //start line
+    5: '#c1f113', //speed boost
+    6: '#2d13f1' //check point
+}
 
 function setColour(value){
     colour = value;
-    alert(colour);
 }
 
 function generateTable(){
@@ -11,7 +19,7 @@ function generateTable(){
     for (var i = 0; i < 100; i++){
         map += "<tr>"
         for (var j = 0; j < 100; j++){
-            map += "<td id='" + i + "-" + j +"' onclick='changeColour(this)'></td>";
+            map += "<td id='" + i + "-" + j +"' onmouseover='changeColour(this)'></td>";
         }
         map += "</tr>";
     }
@@ -20,9 +28,30 @@ function generateTable(){
 }
 
 function changeColour(data){
-    alert(data.id);
+    if (record){
+        data.style.backgroundColor = colourMap[colour];
+    }  
+}
+
+function toggleRecord(){
+    if (record){
+        record = false;
+        document.getElementById('recordButton').innerHTML = "Click to draw";
+    } else {
+        record = true;
+        document.getElementById('recordButton').innerHTML = "Click to stop drawing";
+    }
 }
 
 $('document').ready(function(){
     generateTable();
+
+    document.body.onkeyup = function(e){
+        e.preventDefault();
+        if(e.keyCode == 32){ //spacebar
+            toggleRecord();
+        }
+    }
 });
+
+
